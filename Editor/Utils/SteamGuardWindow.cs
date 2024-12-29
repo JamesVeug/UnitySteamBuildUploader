1847234a9fb7825446c428f45f98e1ca7a25a261
James <james.veugelaers@outlook.co.nz>
2024-12-29 22:25:20 +1300

    SteamGuard window now appears in the middle of the screen and has larger text.

--- Editor/Utils/SteamGuardWindow.cs
+++ Editor/Utils/SteamGuardWindow.cs
@@ -10,10 +10,22 @@ public class SteamGuardWindow : EditorWindow
     private string enteredText;
     private bool waitingForCode;
     
+    [MenuItem("Window/Steam Guard")]
+    public static void ShowWindow()
+    {
+        ShowAsync((S) => { });
+    }
+    
 
     public static async Task ShowAsync(Action<string> codeCallback)
     {
         var window = GetWindow<SteamGuardWindow>();
+        window.titleContent = new GUIContent("Steam Guard Verification");
+        Rect windowPosition = window.position;
+        windowPosition.size = new Vector2(300, 200);
+        windowPosition.center = new Rect(0f, 0f, Screen.currentResolution.width, Screen.currentResolution.height).center;
+        window.position = windowPosition;
+        
         window.enteredText = "";
         window.guardCodeCallback = codeCallback;
         window.waitingForCode = true;
@@ -25,6 +37,10 @@ public class SteamGuardWindow : EditorWindow
 
     private void OnDisable()
     {
+        if (waitingForCode)
+        {
+            guardCodeCallback?.Invoke(null);
+        }
         guardCodeCallback = null;
         waitingForCode = false;
     }
@@ -34,12 +50,21 @@ public class SteamGuardWindow : EditorWindow
         GUILayout.Label("Looks like you need to verify Steam Guard!\n" +
                         "Enter the Steam Guard code below to continue.");
 
-        enteredText = GUILayout.TextField(enteredText);
+
+        // Draw TextField with large text to emphasise the steam guard
+        var largeText = new GUIStyle(GUI.skin.textField) {fontSize = 40};
+        enteredText = GUILayout.TextField(enteredText, largeText);
         
-        if (GUILayout.Button("Retry login"))
+        GUILayout.FlexibleSpace();
+
+        using (new EditorGUI.DisabledScope(enteredText.Length < 1))
         {
-            guardCodeCallback?.Invoke(enteredText);
-            Close();
+            if (GUILayout.Button("Retry login", GUILayout.Height(50)))
+            {
+                guardCodeCallback?.Invoke(enteredText.Trim());
+                waitingForCode = false;
+                Close();
+            }
         }
     }
 }

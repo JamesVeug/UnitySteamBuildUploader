5fee72df9ef84e8ceef7d0bedd5043c00e6f6e91
James <james.veugelaers@outlook.co.nz>
2025-03-18 22:28:18 +1300

    Added ProjectEditorPrefs to separate all editor prefs

---
+++ Editor/Core/Utils/ProjectEditorPrefs.cs
@@ -0,0 +1,57 @@
+﻿namespace Wireframe
+{
+    internal class ProjectEditorPrefs
+    {
+        private static readonly string ProjectKeySuffix = UnityEditor.PlayerSettings.productName + "_";
+
+        public static void SetBool(string key, bool value)
+        {
+            UnityEditor.EditorPrefs.SetBool(ProjectKeySuffix + key, value);
+        }
+
+        public static bool GetBool(string key, bool defaultValue = false)
+        {
+            return UnityEditor.EditorPrefs.GetBool(ProjectKeySuffix + key, defaultValue);
+        }
+
+        public static void SetInt(string key, int value)
+        {
+            UnityEditor.EditorPrefs.SetInt(ProjectKeySuffix + key, value);
+        }
+
+        public static int GetInt(string key, int defaultValue = 0)
+        {
+            return UnityEditor.EditorPrefs.GetInt(ProjectKeySuffix + key, defaultValue);
+        }
+
+        public static void SetString(string key, string value)
+        {
+            UnityEditor.EditorPrefs.SetString(ProjectKeySuffix + key, value);
+        }
+
+        public static string GetString(string key, string defaultValue = "")
+        {
+            return UnityEditor.EditorPrefs.GetString(ProjectKeySuffix + key, defaultValue);
+        }
+
+        public static void SetFloat(string key, float value)
+        {
+            UnityEditor.EditorPrefs.SetFloat(ProjectKeySuffix + key, value);
+        }
+
+        public static float GetFloat(string key, float defaultValue = 0.0f)
+        {
+            return UnityEditor.EditorPrefs.GetFloat(ProjectKeySuffix + key, defaultValue);
+        }
+
+        public static void DeleteKey(string key)
+        {
+            UnityEditor.EditorPrefs.DeleteKey(ProjectKeySuffix + key);
+        }
+
+        public static bool HasKey(string key)
+        {
+            return UnityEditor.EditorPrefs.HasKey(ProjectKeySuffix + key);
+        }
+    }
+}

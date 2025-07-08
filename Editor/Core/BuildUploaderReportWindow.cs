874ce6cda348eaea424a511a797dabbd44ddd8aa
James <james.veugelaers@outlook.co.nz>
2025-07-08 21:21:17 +1200

    Can now open the Build Report window from Window/Build Uploader/Build Reports and load other reports

--- Editor/Core/BuildUploaderReportWindow.cs
+++ Editor/Core/BuildUploaderReportWindow.cs
@@ -7,8 +7,16 @@ namespace Wireframe
     {
         private BuildTaskReport report;
 
+        private string path;
+        
         private string txt;
         private Vector2 scroll;
+
+        [MenuItem("Window/Build Uploader/Build Reports", false, 0)]
+        private static void ShowWindow()
+        {
+            ShowWindow(null, null);
+        }
 
         public static void ShowWindow(BuildTaskReport report, string taskReport=null)
         {
@@ -18,33 +26,65 @@ namespace Wireframe
             windowPosition.size = new Vector2(Screen.currentResolution.width * 0.5f, Screen.currentResolution.height * 0.5f);
             windowPosition.center = new Rect(0f, 0f, Screen.currentResolution.width, Screen.currentResolution.height).center;
             window.position = windowPosition;
-            
+
+            window.path = null;
             window.report = report;
-            window.txt = taskReport ?? report.GetReport();
+            window.txt = taskReport != null ? taskReport : report != null ? report.GetReport() : "";
             window.ShowPopup();
         }
 
         private void OnGUI()
         {
-            if (report == null)
+            // UI to select a report to from your directory
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("Path", GUILayout.Width(50));
+                path = EditorGUILayout.TextField(path);
+                if (GUILayout.Button("...", GUILayout.Width(50)))
+                {
+                    string selectedFile = EditorUtility.OpenFilePanel("Select BuildReport", Preferences.CacheFolderPath, "txt");
+                    if (!string.IsNullOrEmpty(selectedFile))
+                    {
+                        LoadFileAtPath(selectedFile);
+                    }
+                }
+            }
+
+
+            if (string.IsNullOrEmpty(txt))
             {
                 EditorGUILayout.LabelField("No report available.");
                 return;
             }
 
-            if (GUILayout.Button("Save"))
-            {
-                string fileName = $"BuildReport_{report.StartTime:yyyy-MM-dd_HH-mm-ss}.txt";
-                string path = EditorUtility.SaveFilePanel("Save Report", "", fileName, "txt");
-                if (!string.IsNullOrEmpty(path))
-                {
-                    System.IO.File.WriteAllText(path, txt);
-                }
-            }
-
             scroll = EditorGUILayout.BeginScrollView(scroll);
             EditorGUILayout.TextArea(txt);
             EditorGUILayout.EndScrollView();
+
+            if (report != null)
+            {
+                if (GUILayout.Button("Save"))
+                {
+                    string fileName = $"BuildReport_{report.StartTime:yyyy-MM-dd_HH-mm-ss}.txt";
+                    string path = EditorUtility.SaveFilePanel("Save Report", "", fileName, "txt");
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        System.IO.File.WriteAllText(path, txt);
+                    }
+                }
+            }
+        }
+
+        private void LoadFileAtPath(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath) || !System.IO.File.Exists(filePath))
+            {
+                Debug.LogError("File does not exist at the specified path.");
+                return;
+            }
+
+            path = filePath;
+            txt = System.IO.File.ReadAllText(path);
         }
     }
 }

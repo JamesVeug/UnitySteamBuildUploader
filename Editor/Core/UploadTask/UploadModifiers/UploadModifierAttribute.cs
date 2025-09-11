29d4f60eb2b4cc61117a7ef6accc79b884bd38c3
James <james.veugelaers@outlook.co.nz>
2025-09-11 22:07:50 +1200

    Modifier Documentation

--- Editor/Core/UploadTask/UploadModifiers/UploadModifierAttribute.cs
+++ Editor/Core/UploadTask/UploadModifiers/UploadModifierAttribute.cs
@@ -2,6 +2,11 @@
 
 namespace Wireframe
 {
+    /// <summary>
+    /// Marks a class as an upload modifier and provides a user friendly name for it
+    /// Used for reflection to find all upload modifiers
+    /// So you will need a parameterless constructor
+    /// </summary>
     public class UploadModifierAttribute : Attribute
     {
         public string DisplayName { get; }

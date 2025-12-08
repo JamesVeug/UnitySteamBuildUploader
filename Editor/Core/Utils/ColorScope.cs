b3e5315467c5c35f389ba9a9121ec7c3ef522b15
James <james.veugelaers@outlook.co.nz>
2025-12-09 09:56:17 +1300

    Color scope because i keep making it and removing it

---
+++ Editor/Core/Utils/ColorScope.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Wireframe
+{
+    internal struct ColorScope : IDisposable
+    {
+        private bool m_Disposed;
+        private Color m_PreviousColor;
+
+        public ColorScope(Color newColor)
+        {
+            this.m_Disposed = false;
+            this.m_PreviousColor = GUI.color;
+            GUI.color = newColor;
+        }
+
+        public ColorScope(float r, float g, float b, float a = 1f)
+            : this(new Color(r, g, b, a))
+        {
+        }
+
+        public void Dispose()
+        {
+            if (this.m_Disposed)
+                return;
+            this.m_Disposed = true;
+            GUI.color = this.m_PreviousColor;
+        }
+    }
+}
